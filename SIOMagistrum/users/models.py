58b5166ff8fd462b8bc27f6075c8b289c058bc9e
Malthael <ma.1thea1.wright@gmail.com>
2025-11-25 15:47:22 +0100

    Ajout du système basique de professeur et Eleve

--- SIOMagistrum/users/models.py
+++ SIOMagistrum/users/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+'''
+Modèle Élève
+'''
+
+class Eleve(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='eleve')
+    pseudo = models.CharField(max_length=50)
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    annee_scolaire = models.IntegerField()
+    option = models.CharField(max_length=50, blank=True, null=True) #Uniquement si annee_scolaire = 2
+
+    def save(self, *args, **kwargs):
+        if self.annee_scolaire != 2:
+            self.option = None
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"{self.pseudo} (Élève)"
+
+'''
+Modèle Matière
+'''
+
+class Matiere(models.Model):
+    nom = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nom
+
+
+'''
+Modèle Professeur
+'''
+
+class Professeur(models.model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='professeur')
+    pseudo = models.CharField(max_length=50)
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    matieres = models.ManyToManyField(Matiere, blank=True)
+
+    def __str__(self):
+        return f"{self.pseudo} (Professeur)"
