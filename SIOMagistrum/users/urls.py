2a3826c7907f9974a1299e48242439c2e27bdaec
Malthael <ma.1thea1.wright@gmail.com>
2025-12-02 15:44:10 +0100

    Ajout de page d'inscription eleve (pas implementé encore)

--- SIOMagistrum/users/urls.py
+++ SIOMagistrum/users/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('professeur/inscription/', views.inscription_prof,  name="inscription_prof"),
     path('professeur/connexion/', views.prof_login, name="login_prof"),
-    path('eleve/connexion/', views.eleve_login, name="eleve_login")
+    path('eleve/connexion/', views.eleve_login, name="login_eleve"),
+    path('eleve/inscription/', views.eleve_login, name="inscription_eleve")
 ]
