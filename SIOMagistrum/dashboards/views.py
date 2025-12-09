ac48ffbecfe9566effa17ae42e7b005044681a90
Malthael <ma.1thea1.wright@gmail.com>
2025-12-09 13:46:37 +0100

    Mise à jour de la vue du dashboard Prof

--- SIOMagistrum/dashboards/views.py
+++ SIOMagistrum/dashboards/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
+from dashboards.models import Cours
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard_prof(request):
     if not request.user.is_authenticated:
         return redirect("prof_login")
-    return render(request, 'dashboards/profs/dashboardprof.html')
+    professeur = request.user.professeur
+    cours = Cours.objects.filter(professeur=professeur)
+    return render(request, 'dashboards/profs/dashboardprof.html', {'cours': cours})
