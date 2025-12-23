8273166a75ba8ecfd054a4eb3aa227ccf411fda9
Malthael <ma.1thea1.wright@gmail.com>
2025-12-23 19:30:46 +0100

    Migrations + ajout des urls dans l'url.py

--- SIOMagistrum/dashboards/urls.py
+++ SIOMagistrum/dashboards/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('dashboard/professeur/modify/<int:pk>/', views.modify_cours, name="modify_cours"),
     path('dashboard/professeur/delete/<int:pk>/', views.delete_cours, name='delete_cours'),
     path('dashboard/professeur/cours/<int:pk>/', views.cours_detail, name='cours_detail'),
-    path('dashboard/professeur/cours/module/modify/<int:pk>/', views.modify_module, name="modify_module"),
-    path('dashboard/professeur/cours/module/delete/<int:pk>/', views.delete_module, name="delete_module")
+    path('dashboard/professeur/cours/modify/<int:pk>/', views.modify_module, name="modify_module"),
+    path('dashboard/professeur/cours/delete/<int:pk>/', views.delete_module, name="delete_module"),
+    path('dashboard/professeur/cours/module/<int:pk>/', views.module_detail, name="module_detail"),
+    path('dashboard/professeur/cours/module/exercices/test-docker/', views.tester_code_docker, name='tester_code_docker')
 ]
